b48488b3fea93869752149fa12eeeeac239d4fe5
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2022-04-18 10:32:25 +0700

    edit config

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-GROUP_ID = 212216280
+GROUP_ID = "your group id. must be integer"
 
 KEYBOARD = {}
 
-TOKEN = "67551833ab9353f9013192edf4394ade976f874adddfc5a3a0a87b08005df6ea3235b09390c2714f26fdd"
+TOKEN = "your token"
 
 KEYBOARD = {
     "one_time": False,
@@ -13,7 +13,7 @@ KEYBOARD = {
                 "label": "Ищем пидра",
                 "payload": "{\"button\": \"1\"}",
             },
-            'color': 'primary'
+            'color': 'positive'
         }, 
         {
             "action": {
@@ -21,8 +21,42 @@ KEYBOARD = {
                 "label": "Ищем красавчика",
                 "payload": "{\"button\": \"2\"}",
             },
-            'color': 'primary'
+            'color': 'negative'
         }
         ],
+        [
+        {
+            "action": {
+                "type": "text",
+                "label": "Топ пидора",
+                "payload": "{\"button\": \"3\"}",
+            },
+            'color': 'secondary'
+        },
+        {
+            "action": {
+                "type": "text",
+                "label": "Топ красавчика",
+                "payload": "{\"button\": \"4\"}",
+            },
+            'color': 'secondary'
+        }
+        ]
     ]
+}
+
+
+
+PIDOR_MESSAGES = ['Кто же тут долбиться в сраку?', 'Проверяю ширину очка каждого', 'Оу, да вам необходимо зашивать...', 'И сегодня пидор - @id{}({} {})']
+
+PRETTY_MESSAGES = ['Красавчики всех стран объединяйтесь!', 'Анализирую красоту по размеру члена/глубине вагины', 'Красавчик сегодня - @id{}({} {})']
+
+COMMAND_CHOICE = {
+    '{"button":"1"}': (PIDOR_MESSAGES, 'last_call_pid', "Пидора", 'count_pid'), 
+    '{"button":"2"}': (PRETTY_MESSAGES, 'last_call_pretty', "Красавчика", 'count_pretty'),
+}
+
+COMMAND_TOP = {
+    '{"button":"3"}': ('Топ Пидора:\n', 4),
+    '{"button":"4"}': ("Топ Красавчика:\n", 5)
 }
